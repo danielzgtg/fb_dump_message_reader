1e44073d2e80e7891afaa945caa75b4a785b2d8c
Daniel Tang <danielzgtg.opensource@gmail.com>
2020-04-30 01:47:02 -0400

    Align the colon

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 extern crate serde_json;
+extern crate unicode_segmentation;
 
 use std::env;
 use std::fs;
 use serde_json::Value;
+use unicode_segmentation::UnicodeSegmentation;
 
 fn main() {
   let filename = env::args().skip(1).next().expect("error getting filename");
@@ -10,13 +12,16 @@ fn main() {
   let parsed: Value = serde_json::from_str(&rawdata).expect("error parsing json");
 
   let messages = &parsed["messages"];
-  for x in messages.as_array().expect("error coercing message array").iter().rev() {
+  let messages = messages.as_array().expect("error coercing message array");
+  if messages.len() == 0 { return }
+  let namewidth = messages.iter().map(|x| x["sender_name"].as_str().expect("expected sender str").graphemes(true).count()).max().unwrap();
+  for x in messages.iter().rev() {
     //let t = &x["type"];
     //if t != "Generic" {
     //  continue;
     //}
-    let c = &x["content"];
-    let s = &x["sender_name"];
-    println!("{}: {}", s, c);
+    let c = x["content"].as_str().unwrap_or("[missing]");
+    let s = x["sender_name"].as_str().unwrap();
+    println!("{:0width$}: {}", s, c, width = namewidth);
   }
 }
