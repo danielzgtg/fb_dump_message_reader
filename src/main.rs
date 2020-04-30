6d1f27c9bdca12ae5b7d592bb20493f6760555b0
Daniel Tang <danielzgtg.opensource@gmail.com>
2020-04-30 01:14:33 -0400

    Get arguments from cmdline

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 extern crate serde_json;
 
+use std::env;
 use std::fs;
 use serde_json::Value;
 
 fn main() {
-  let rawdata = fs::read_to_string("./message_1.json").expect("error reading ./data.json");
+  let filename = env::args().skip(1).next().expect("error getting filename");
+  let rawdata = fs::read_to_string(filename).expect("error reading ./data.json");
   let parsed: Value = serde_json::from_str(&rawdata).expect("error parsing json");
 
   let messages = &parsed["messages"];
